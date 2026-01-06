fb86d7fc1ec2ba01d9167a035673b63119ec9e17
StillBlack <stillblack2014@gmail.com>
2026-01-06 12:28:38 +0500

    day1 done

---
+++ Day1/day1,part1.dart
@@ -0,0 +1,21 @@
+void main() {
+  var isLoggedIn = false;
+  isLoggedIn = true;
+  print('Result $isLoggedIn');
+
+  const appTitle = 'My APP';
+  final userId = 467433;
+  var cartItemCount = 09;
+
+  print('$userId, $appTitle, $cartItemCount');
+  print(appTitle);
+  print(userId);
+  print(cartItemCount);
+
+  final int age = 24;
+  print(age * 4);
+
+  var nickName = 'GORGAN';
+  nickName = 'Ali';
+  print(nickName);
+}
---
+++ Day1/day1.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('Day 1 started ');
+
+  var serialNumber = 0;
+  for (var i = 0; i < 5; i++) {
+    serialNumber++;
+    print('serialNumber $serialNumber');
+
+    final nickName = 'Gorgan';
+    // nickName = 'haider';
+    print(nickName);
+
+    const appVersion = 1.0;
+    // appVersion = 2.0;
+    print(appVersion);
+  }
+}
