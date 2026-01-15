629f654a90cc3e922d6d089f156054115097fbf9
StillBlack <stillblack2014@gmail.com>
2026-01-15 19:07:08 +0500

    day 4 nws exercise done

--- Day4/part2.dart
+++ Day4/part2.dart
@@ -1,8 +1,15 @@
 void main() {
   int income = calculateIncome(150);
   print(income);
-  int add =sum(45, 67);
+  int add = sum(45, 67);
   print(add);
+  intro('ejaz');
+  intro('Gorgan');
+  int result = getGreater(45, 67);
+  print(result);
+
+  String checkPerson = getAgeStatus(55);
+  print(checkPerson);
 }
 
 int calculateIncome(int intialincome) {
@@ -12,4 +19,30 @@ int calculateIncome(int intialincome) {
 
 int sum(int num1, int num2) {
   return (num1 + num2);
+}
+
+void greet() {
+  print('welcome to flutter');
+}
+
+void intro(String name) {
+  print('hello $name');
+}
+
+int getGreater(int a, int b) {
+  if (a > b) {
+    print('$a is greater');
+    return a;
+  } else {
+    print('$b is greater ');
+    return b;
+  }
+}
+
+String getAgeStatus(int a) {
+  if (a >= 18) {
+    return 'Adult';
+  } else {
+    return 'Minor';
+  }
 }
