7978ad4a6bdcea39ffe8ab2946969cd0bede61f1
StillBlack <stillblack2014@gmail.com>
2026-01-26 12:57:03 +0500

    half done day5

--- Day5/part1.dart
+++ Day5/part1.dart
@@ -4,8 +4,13 @@ void main() {
   print(num[1]);
   print(num[3] = 45);
   print(num);
-  for (int i = 0; i < students.length; i++) {
+  for (int i = 1; i < students.length; i++) {
     print('Students : ${students[i]}');
+  }
+  for (int i = 1; i < numbers.length; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
   }
 }
 
@@ -14,3 +19,5 @@ List<String> fruits = ['apple', 'banana', 'graps ', 'strawberry', 'berry'];
 List<int> num = [10, 20, 30, 40, 50];
 
 List<String> students = ['ALi', 'Hussain', 'ejaz', 'gorgan', 'yazdan'];
+
+List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
