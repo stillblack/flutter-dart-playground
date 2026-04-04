a4e8a0bfbc3de54838a7c25b4484e03329442712
StillBlack <stillblack2014@gmail.com>
2026-04-04 17:21:18 +0500

    day 7  part 1 done

---
+++ Day7/part1.dart
@@ -0,0 +1,12 @@
+void main() {
+  String fullNAme = '    Ejaz Ali     ';
+  print(fullNAme.trim());
+
+  String name = '';
+  // print(name.isEmpty);
+  if (name.isEmpty) {
+    print('Invalid name');
+  }
+  String email = 'ejazgorgan@yahoo.com';
+  print(email.contains('@gmail.com'));
+}
