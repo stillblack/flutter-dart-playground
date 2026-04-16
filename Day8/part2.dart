f350bfbe8d9ca80b4777abadc40c02d163ec3cdc
StillBlack <stillblack2014@gmail.com>
2026-04-16 18:00:43 +0500

    level is completed

--- Day8/part2.dart
+++ Day8/part2.dart
@@ -1,19 +1,35 @@
 void main() {
+  // 2
   for (Map i in students) {
-    if (i['age'] >= 18) {
-      print('Name ${i['name']} ,' 'Age ${i['age']}');
 
-    }else{
-      print('Name ${i['name']} ,' 'Age ${i['age']} is not allowed to enter');
+    print('${i['name']} ');
+  }
+  for (Map i in students) {
+    // 2
+    if (i['age'] >= 18) {
+      print(
+        'Name ${i['name']} ,'
+        'Age ${i['age']}',
+      );
+    } else {
+      print(
+        'Name ${i['name']} ,'
+        'Age ${i['age']} is not allowed to enter',
+      );
     }
   }
 }
 
-// level 2
 List<Map<String, dynamic>> students = [
-  {'name': 'Ejaz Ali', 'age': 21},
-  {'name': 'haider', 'age': 12},
-  {'name': 'abbs Ali', 'age': 17},
-  {'name': ' Ali shah', 'age': 18},
-  {'name': ' shah G', 'age': 27},
+  {'name': 'Ali', 'age': 17},
+  {'name': 'Ahmed', 'age': 22},
+  {'name': 'Sara', 'age': 19},
 ];
+// level 2
+// List<Map<String, dynamic>> students = [
+//   {'name': 'Ejaz Ali', 'age': 21},
+//   {'name': 'haider', 'age': 12},
+//   {'name': 'abbs Ali', 'age': 17},
+//   {'name': ' Ali shah', 'age': 18},
+//   {'name': ' shah G', 'age': 27},
+// ];
