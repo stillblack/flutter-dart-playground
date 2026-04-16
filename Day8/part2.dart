8d867950ff6e74c19f1320f4ff82bcceb4bf87a8
StillBlack <stillblack2014@gmail.com>
2026-04-16 17:52:42 +0500

    level 1 done

---
+++ Day8/part2.dart
@@ -0,0 +1,19 @@
+void main() {
+  for (Map i in students) {
+    if (i['age'] >= 18) {
+      print('Name ${i['name']} ,' 'Age ${i['age']}');
+
+    }else{
+      print('Name ${i['name']} ,' 'Age ${i['age']} is not allowed to enter');
+    }
+  }
+}
+
+// level 2
+List<Map<String, dynamic>> students = [
+  {'name': 'Ejaz Ali', 'age': 21},
+  {'name': 'haider', 'age': 12},
+  {'name': 'abbs Ali', 'age': 17},
+  {'name': ' Ali shah', 'age': 18},
+  {'name': ' shah G', 'age': 27},
+];
