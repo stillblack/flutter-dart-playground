a52bd1e48cd975594b237e8152ac00d878842143
StillBlack <stillblack2014@gmail.com>
2026-04-27 04:40:22 +0500

    task two done

--- Day9/level_one.dart
+++ Day9/level_one.dart
@@ -2,8 +2,11 @@ void main() {
   print(user);
   print('\n');
   print(getUserAge());
+  print('\n');
+  print(countAdmin(details));
 }
 
+// Levle one
 List<Map<String, dynamic>> user = [
   {"name": "Alice", "age": 30},
   {"name": "Bob", "age": 25},
@@ -18,4 +21,21 @@ List<Map<String, dynamic>> getUserAge() {
     }
   }
   return result;
+}
+
+// Task two
+List<Map<String, dynamic>> details = [
+  {'name': 'Alice', 'age': 30, 'city': 'New York', 'job': 'Engineer'},
+  {'name': 'Bob', 'age': 25, 'city': 'Los Angeles', 'job': 'Admin'},
+  {'name': 'Charlie', 'age': 35, 'city': 'Chicago', 'job': 'Manager'},
+  {'name': 'barbar', 'age': 13, 'city': 'china', 'job': 'admin'},
+];
+int countAdmin(List details) {
+  int count = 0;
+  for (var i in details) {
+    if (i['job'] == 'Admin' || i['job'] == 'admin') {
+      count++;
+    }
+  }
+  return count;
 }
