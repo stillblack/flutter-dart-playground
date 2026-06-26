eaff0c4c7791a90c2ddd7604c35ab937350a7676
StillBlack <stillblack2014@gmail.com>
2026-06-26 23:58:46 +0500

    day 12 started

---
+++ Day12/level_one.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Ejaz";
+  int age = 22;
+  double height = 5.9;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Student: $isStudent");
+}
