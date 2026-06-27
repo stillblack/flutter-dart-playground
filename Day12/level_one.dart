9d89b5f0a50fcabaa371e887e6db3aab074c2256
StillBlack <stillblack2014@gmail.com>
2026-06-28 04:55:25 +0500

    exercise 3 completed

--- Day12/level_one.dart
+++ Day12/level_one.dart
@@ -1,5 +1,24 @@
-void main() async {
+void main() {
+  checkData();
+  print('\n');
+  Login();
+}
+
+void checkData() async {
   print('loading ');
   await Future.delayed(Duration(seconds: 3));
   print('Data Loaded');
+  String result = await loadedProducts();
+  print(result);
+}
+
+void Login() async {
+  print('Login');
+  await Future.delayed(Duration(seconds: 2));
+  print('Login Successfully');
+}
+
+Future<String> loadedProducts() async {
+  await Future.delayed(Duration(seconds: 5));
+  return 'Products Loaded';
 }
