49a6c11979ce3005df8958e1d6b850147633bf38
StillBlack <stillblack2014@gmail.com>
2026-06-28 04:35:39 +0500

    day 12 async , await and future task started

--- Day12/level_one.dart
+++ Day12/level_one.dart
@@ -1,11 +1,5 @@
-void main() {
-  String name = "Ejaz";
-  int age = 22;
-  double height = 5.9;
-  bool isStudent = true;
-
-  print("Name: $name");
-  print("Age: $age");
-  print("Height: $height");
-  print("Student: $isStudent");
+void main() async {
+  print('loading ');
+  await Future.delayed(Duration(seconds: 3));
+  print('Data Loaded');
 }
