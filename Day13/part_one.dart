b6a0a209128b204ed6d29d05832e03adc8d43098
StillBlack <stillblack2014@gmail.com>
2026-07-15 03:10:08 +0500

    part one completed

--- Day13/part_one.dart
+++ Day13/part_one.dart
@@ -8,4 +8,23 @@ void main() {
     print('an error occured');
   }
   print('program ended');
+
+  print('\n');
+  try {
+    int result = 20 ~/ 0;
+    print(result);
+  } catch (e) {
+    // print('an error occured');
+    print(e);
+  }
+  //   print('program fin')
+  try {
+    print('connecting ...');
+    int result = 10 ~/ 0;
+    print(result);
+  } catch (e) {
+    print('connection failed');
+  } finally {
+    print('connection closed');
+  }
 }
