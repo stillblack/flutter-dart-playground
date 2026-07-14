e9c8bd4e15f3efbeb80a9547b5c93b60f397d046
StillBlack <stillblack2014@gmail.com>
2026-07-14 23:45:50 +0500

    exercise is completed

--- Day13/part_one.dart
+++ Day13/part_one.dart
@@ -1,1 +1,15 @@
-void main(){}
+void main() {
+  int learn = 3;
+
+  int num = 7;
+  int score = 70;
+  if (score >= 60 && score == 70) {
+    print('you are qualified');
+  } else {
+    print('sorry you are failed');
+  }
+  print('\n');
+  for (int i = 0; i <= num; i++) print(i);
+  print('\n');
+  for (int i = 0; i == learn; i++) print('love');
+}
